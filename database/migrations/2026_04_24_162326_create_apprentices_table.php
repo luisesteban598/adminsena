e854d0a71d904d07b0d65649af63516ed91d18b5
luis <Stebannoriega598@gmail.com>
2026-04-24 11:58:55 -0500

    migraciones

---
+++ database/migrations/2026_04_24_162326_create_apprentices_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apprentices', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('cell number');
+
+            $table->unsignedBigInteger('course_id')->nullable()->unique();
+
+            $table->foreign('course_id')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+            
+            
+            $table->unsignedBigInteger('computer_id')->nullable()->unique();
+
+            $table->foreign('computer_id')
+                ->references('id')
+                ->on('computers')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apprentices');
+    }
+};
